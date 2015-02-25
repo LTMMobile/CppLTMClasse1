f07cbe3d24ddab7f4090570aef586e2567871f28
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2015-02-25 12:09:49 +0100

    Initial Commit

---
+++ LTMClasse1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LTMClasse1
+//
+//  Created by bleclercq on 25/02/2015.
+//  Copyright (c) 2015 LTM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
